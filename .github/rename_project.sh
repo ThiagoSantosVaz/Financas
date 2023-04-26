ea2f63a99c5434422b78bb6dd149dc6f98472682
ThiagoSantosVaz <ThiagoSantosVaz@users.noreply.github.com>
2023-04-26 02:55:18 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="ThiagoSantosVaz"
+original_name="financas"
+original_urlname="Financas"
+original_description="Awesome financas created by ThiagoSantosVaz"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv financas $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from financas import create_app
+from financas.ext.commands import populate_db
+from financas.ext.database import db
 
 
 @pytest.fixture(scope="session")
